ef5bfa9c177b94fec9c140f394e8b8afdbbbcd03
David <touisbetterthanpi@gmail.com>
2021-01-07 22:47:30 -0600

    Add stencil testing and use the rect as the stencil

--- src/main.rs
+++ src/main.rs
@@ -157,29 +157,47 @@ fn main() {
         }
 
         gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+        gl::StencilMask(0xFF); //enable writing to the mask (including gl::Clear)
         gl::Clear(gl::COLOR_BUFFER_BIT|gl::STENCIL_BUFFER_BIT);
+        gl::StencilMask(0x00); //disable writing to the mask just as cleanup
 
-        gl::Enable(gl::STENCIL_TEST); 
-        gl::StencilOp(gl::KEEP, gl::REPLACE, gl::REPLACE);  
-        gl::StencilMask(0xFF);
+
+        gl::Enable(gl::STENCIL_TEST); //enable stencil test 
+
+//Draw to the mask our stencil
+
         
-
-
-        // gl::ClearColor()
-
-
-
-        background_program.bind();
-        background.draw(gl::TRIANGLES);
-        background_program.unbind();
-
-        print_errors(149);
-
+        gl::StencilOp(gl::KEEP, gl::REPLACE, gl::REPLACE); //write to the mask, ignoring depth
+        gl::StencilFunc(gl::ALWAYS, 1, 0xFF); //anything that passes, write 1 (i think)
+        gl::StencilMask(0xFF); //this (weirdly) allows the mask to be written to
+        
+        gl::ColorMask(gl::FALSE,gl::FALSE,gl::FALSE,gl::FALSE); //dont write to color
+        gl::DepthMask(gl::FALSE); //dont write to depth
 
         rect_program.bind();
         rect.draw(gl::TRIANGLE_STRIP);
         rect_program.unbind();
         
+        gl::StencilMask(0x00); //disable writing to the mask
+        gl::ColorMask(gl::TRUE,gl::TRUE,gl::TRUE,gl::TRUE); //re-enable writing to color
+        gl::DepthMask(gl::TRUE); //re-enable writing to depth
+
+//Draw what the stencil is applied to  (the background)
+        //If the mask value at that fragment is gl::Equal to 1 after its && with 0xFF,
+        //draw the fragment
+        gl::StencilFunc(gl::EQUAL, 1, 0xFF); 
+
+        background_program.bind();
+        background.draw(gl::TRIANGLES);
+        background_program.unbind();
+
+
+
+
+        print_errors(149);
+
+
+
 
 
         // check and call events and swap the buffers
